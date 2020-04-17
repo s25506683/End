3753249facf6d956f95c1ee4b7d3215f78eeb456
jay18309 <cookopen2>
2020-04-17 20:48:39 +0800

    4/17請假條件更新

--- src/main/java/com/example/demo/SecurityConfig1.java
+++ src/main/java/com/example/demo/SecurityConfig1.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import java.net.InetAddress;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.sql.DataSource;
 
@@ -15,68 +17,52 @@ import org.springframework.security.crypto.bcrypt.*;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
 @EnableWebSecurity
 @Configuration
-@Order(2)
-public class SecurityConfig3 extends WebSecurityConfigurerAdapter {
+@Order(1)
+public class SecurityConfig1 extends WebSecurityConfigurerAdapter {
 
-	public SecurityConfig3() {
+	public SecurityConfig1() {
         super();
     }
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	protected void configure(final HttpSecurity http) throws Exception {
 		InetAddress ip;
 		ip = InetAddress.getLocalHost();
 		System.out.println(ip.getHostAddress());
-		
+
 		http.httpBasic().and().authorizeRequests()     //例外處理
-        .antMatchers("/css/**", "/index").permitAll()
+		.antMatchers("/css/**", "/index").permitAll()
 		.antMatchers("/teacher/**").hasRole("ADMIN")
 		.antMatchers("/student/**").hasRole("USER")
-		.and().rememberMe().tokenValiditySeconds(100).and().cors()
+		.and().rememberMe().tokenValiditySeconds(20).and().cors()
 //		.anyRequest().denyAll()    //除了上述條件以外全部擋住
-		.and().csrf().disable()   //關掉跨網站的請求(避免回傳錯誤403)
+		.and().csrf().disable()  //關掉跨網站的請求(避免回傳錯誤403
+		
+
 		
 		.formLogin().loginPage("/login").defaultSuccessUrl("http://" + ip.getHostAddress() + ":3000/homepage1").failureUrl("http://" + ip.getHostAddress() + ":3000").permitAll()
   		.and()
   		.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("http://" + ip.getHostAddress() + ":3000");
-		
+
 	}
 
 	@Autowired
 	private DataSource dataSource;
 
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		String password = passwordEncoder.encode("12345");
+	public void configureGlobal(final AuthenticationManagerBuilder auth) throws Exception {
+		final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		final String password = passwordEncoder.encode("12345");
 		System.out.println(password);
 		
 		auth.jdbcAuthentication().dataSource(dataSource)
-		.usersByUsernameQuery("select teacher_id , teacher_password, enabled from teacher where teacher_id=?")
-		.authoritiesByUsernameQuery("select teacher_id, role from teacher where teacher_id=?")
+		.usersByUsernameQuery("select std_id , std_password, enabled from student where std_id=?")
+		.authoritiesByUsernameQuery("select std_id, role from student where std_id=?")
 		.passwordEncoder(passwordEncoder);
-					
-		
-//		auth.inMemoryAuthentication()     // 驗證資訊存放於記憶體
-//        .passwordEncoder(passwordEncoder)
-//        .withUser("admin") 
-//            .password(passwordEncoder.encode("admin12345678"))
-//            .roles("ADMIN", "MEMBER")
-//        .and()
-//        .withUser("caterpillar")
-//            .password(passwordEncoder.encode("12345678"))
-//            .roles("MEMBER");
 
-		
-		
-		
-
-		
-		
-		
-		
 		
 	}
 
