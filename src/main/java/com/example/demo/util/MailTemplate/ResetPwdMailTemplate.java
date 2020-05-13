ed338ff9f167645cb70a21d75a230aadb2523fad
Carson <s25506683@gmail.com>
2020-05-13 22:08:46 +0800

    new mailtemplate folder.

--- src/main/java/com/example/demo/util/MailTemplate/ResetPwdMailTemplate.java
+++ src/main/java/com/example/demo/util/MailTemplate/ResetPwdMailTemplate.java
@@ -1,4 +1,4 @@
-package com.example.demo.util;
+package com.example.demo.util.MailTemplate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
