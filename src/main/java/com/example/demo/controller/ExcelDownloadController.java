48f02ea2f84dcb0f5717091b1a9da9a078f37ab7
Carson <s25506683@gmail.com>
2020-05-30 15:02:34 +0800

    excel.

--- src/main/java/com/example/demo/controller/ExcelDownloadController.java
+++ src/main/java/com/example/demo/controller/ExcelDownloadController.java
@@ -1,29 +1,18 @@
 package com.example.demo.controller;
 
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
-import javax.activation.MimetypesFileTypeMap;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 
 import com.example.demo.dao.ExcelDownloadDAO;
-import com.example.demo.entity.ExcelDownload;
 import com.example.demo.util.ExcelUtil;
 import com.example.demo.util.AuthenticationUtil;
 import com.example.demo.util.Logfile;
